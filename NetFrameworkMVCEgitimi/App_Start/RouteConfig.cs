0932ed0c426e50f7cf9f98ff2fc80a262a3a37d3
Mesut ILICA <mesut.ilica@gmail.com>
2025-11-16 14:00:48 +0300

    16-11-25 mvc start

---
+++ NetFrameworkMVCEgitimi/App_Start/RouteConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace NetFrameworkMVCEgitimi
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}", // url kısmı uygulama tarayıcıda çalıştığında site adından sonraki kısımda hangi controllerdaki hangi action ın çalıştırılacağını ayarlar.
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional } // defaults kısmı uygulama çalıştığında varsayılan anasayfa olarak hangi sayfanın çalıştırılacağını ayarlar.
+            );
+        }
+    }
+}
